bd6dbdbd61e8e7e06d129b5bb9b41c82cc05fa3c
Gladyskyenze <kyenzegladys@gmail.com>
2022-03-11 19:19:51 +0300

    changing the outlook of html

--- jscript/script.js
+++
@@ -1,7 +0,0 @@
-function displaynames(){
-  let dates = document.getElementById("date").ariaValueMax;
-  let months = document.getElementById("monthh").ariaValueMax;
-  let yearr = document.getElementById("yearr").ariaValueMax;
-  let gender = document.getElementById("gender").ariaValueMax;
-}
-  document.write("result");
