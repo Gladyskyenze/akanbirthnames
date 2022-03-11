9dfc632c5b4400e0d6c402351ba54cae3cf77a4b
Gladyskyenze <kyenzegladys@gmail.com>
2022-03-11 11:47:35 +0300

    changes to the js

--- jscript/script.js
+++ jscript/script.js
@@ -1,23 +1,7 @@
-
-function getAkanames() {
-    var d1 = document.getElementById("date").value;
-    var m1 = document.getElementById("month").value;
-    var y1 = document.getElementById("year").value;
-    var genders = document.getElementById("gender").value;
-    
-  
-    // function to get gender
-    function getGender () {
-      for (let gender of genders){
-        if (gender.checked){
-          return gender.value;
-        }
-      }
-    }
-  
-    var myGenderValue = getGender();
-    console.log(myGenderValue);
-  
-    
-  }
-  
+function displaynames(){
+  let dates = document.getElementById("date").ariaValueMax;
+  let months = document.getElementById("monthh").ariaValueMax;
+  let yearr = document.getElementById("yearr").ariaValueMax;
+  let gender = document.getElementById("gender").ariaValueMax;
+}
+  document.write("results")
